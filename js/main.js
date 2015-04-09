9772c4d7b0f02d695b55bb1a15c6b5b355760bee
jnthn18 <jnthn18@gmail.com>
2015-04-09 00:02:29 -0400

    Styled the menu for creating a playlist

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@ $(document).ready(function() {
 	var p1_song = ["Dani California","Shake It Off","ATLiens","Dani California","Shake It Off","ATLiens"];
 	var playlist_1 = [p1_songID, p1_favorite, p1_songservice, p1_artist, p1_song];
 
+	var p2_favorite = [0,1,1,0,1];
+	var p2_songservice = [2,3,1,3,1,1];
+	var playlist_2 = [p1_songID, p2_favorite, p2_songservice, p1_artist, p1_song];
+
+	var list = [playlist_1, playlist_2];
+
 	function createPlaylist(p){
 		var playlistLength = p[0].length;
 		$(".playlist").css("display", "none").css("opacity", 0);
@@ -46,9 +52,9 @@ $(document).ready(function() {
 		}
 	});
 	$(".frame").click(function() {
-		if ( $("#playlist-menu").css("display") !== 'none'){
+/*		if ( $("#playlist-menu").css("display") !== 'none'){
 			$("#playlist-menu").velocity("slideUp", {duration: 300});
-		}
+		}*/
 	});
 
 	$("#playlist-menu li").click(function() {
@@ -56,14 +62,26 @@ $(document).ready(function() {
 		var listID = $(this).index();
 
 		if( listID === listLength ){
-			alert("create playlist");
+		// alert("create playlist");
+/*			var lastList = $("#playlist-menu li").length - 3;
+			var newList = "<li>"+"Hanging Out"+"</li>";
+			$("#playlist-menu li").eq(lastList).after(newList);*/
+		var menuWidth = $("#playlist-menu").width();
+		var menuHeight = $("#playlist-menu").height();
+		$("#playlist-menu").css("height", menuHeight);
+		$("#playlist-menu").velocity("transition.slideDownOut", {duration: 300});
+		$(".playlist-form").css("width", menuWidth).velocity("transition.slideDownIn");
+		$(".playlist-form-wrapper").velocity("transition.slideDownIn", {duration: 300, delay: 200});
 		} else if ( listID === listLength - 1){
 
 		} else {
 			var playlist = $("#playlist-menu li").eq(listID).html();
 			if ( $("#playlist-title").html() !== playlist ){
 				$("#playlist-title").velocity("transition.slideRightOut", {duration: 200});
+				$(".playlist").empty();
+				createPlaylist(list[listID]);
 				setTimeout(function() {
+					
 					$("#playlist-title").html(playlist).velocity("transition.slideLeftIn", {duration: 200});
 				}, 210);
 				$("#playlist-menu").velocity("slideUp", {duration: 300});				
